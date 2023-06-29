a5c01214f33dd1135a646ad74ea3a27a5d8a9218
Rikerdaves <rikerdaves.alencar@gmail.com>
2023-06-29 10:59:35 -0300

    App Filmes

--- AppFilmes/src/screen/Home.js
+++ AppFilmes/src/screen/Home.js
@@ -4,24 +4,63 @@ import MovieSlide from '../components/MovieSlide';
 import SearchBar from '../components/Searchbar';
 import MovieDetails from '../screen/MovieDetails';
 import SeriesSlide from '../components/SeriesSlide';
+import SeriesDetails from './SeriesDetails';
+import MovieList from '../components/Cardfilms';
+import Menu from '../components/Menu';
 
 const Home = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [selectedSeries, setSelectedSeries] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
+  const [showMovieList, setShowMovieList] = useState(false);
+
+  const handleGoBack = () => {
+    setSelectedMovie(null);
+    setSelectedSeries(null);
+    setShowDetails(false);
+    setShowMovieList(false);
+  };
+
+  const handleMenuPress = (screen) => {
+    if (screen === 'movielist') {
+      setShowDetails(false);
+      setSelectedMovie(null);
+      setSelectedSeries(null);
+      setShowMovieList(true);
+    } else {
+      setShowDetails(false);
+      setSelectedMovie(null);
+      setSelectedSeries(null);
+      setShowMovieList(false);
+    }
+  };
 
   return (
-    <>
     <View style={styles.container}>
-      {!showDetails && <Text style={styles.texto}>O que deseja assistir?</Text>}
-      {!showDetails && <SearchBar />}
-      {showDetails ? (
-        <MovieDetails movie={selectedMovie} handleGoBack={() => setShowDetails(false)} />
-      ) : (
-        <MovieSlide setSelectedMovie={setSelectedMovie} setShowDetails={setShowDetails} />
+      <Menu handleMenuPress={handleMenuPress} />
+      {!showDetails && !showMovieList && (
+        <>
+          <Text style={styles.texto}>O que deseja assistir?</Text>
+          <SearchBar />
+          <MovieSlide setSelectedMovie={setSelectedMovie} setShowDetails={setShowDetails} />
+          <SeriesSlide setSelectedSeries={setSelectedSeries} setShowDetails={setShowDetails} />
+        </>
       )}
-      <SeriesSlide />
+      {showDetails && (
+        <>
+          {selectedMovie ? (
+            <MovieDetails movie={selectedMovie} handleGoBack={handleGoBack} />
+          ) : (
+            <SeriesDetails series={selectedSeries} handleGoBack={handleGoBack} />
+          )}
+        </>
+      )}
+      {showMovieList && (
+        <View style={styles.movieListContainer}>
+          <MovieList />
+        </View>
+      )}
     </View>
-    </>
   );
 };
 
@@ -32,11 +71,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#313131',
   },
   texto: {
-    paddingTop: 30,
+    paddingTop: 10,
     marginLeft: 20,
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+  },
+  movieListContainer: {
+    flex: 1,
   },
 });
 
